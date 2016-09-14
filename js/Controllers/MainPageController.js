4fc1dd6a9edb45bc80c51d271065a114ab2d038e
Chakalaka11 <dyomin_vic@ukr.net>
2016-09-14 19:26:42 +0300

    Initial commit

---
+++ js/Controllers/MainPageController.js
@@ -0,0 +1,47 @@
+
+var url = "http://localhost:51296/api/default/getContent?callback=JSON_CALLBACK";
+
+
+app.controller('MainPageContr', function ($scope, $http) {
+    $scope.directories = [];
+    $scope.files = [];
+    $scope.title = 'File Manager layout';
+    $scope.group = [0, 0, 0];
+    $scope.absolutePath = {};
+    $scope.bars = [
+        'Less 10 Mb',
+        '10 Mb - 50 Mb',
+        'More 100 Mb'];
+    $scope.goto = function (event) {
+        if (event != null) {
+            var clicked = event.target.childNodes[0].data;
+            console.log(clicked);
+            if (clicked == "../") {
+                var preEndSlashIndex =$scope.absolutePath.lastIndexOf("\\",$scope.absolutePath.length-2)+1;
+                $scope.absolutePath = $scope.absolutePath.substring(0, preEndSlashIndex);
+
+            }
+            else {
+                $scope.absolutePath = $scope.absolutePath  + clicked+ "\\";
+            }
+        }
+        request();
+    };
+    function request() {
+        $http.post(url, { path: $scope.absolutePath }).success(function (data, status, headers, config) {
+            $scope.directories = data.Directories;
+            $scope.files = data.Files;
+            $scope.group = data.SizeGroups;
+        }).error(function (data, status, headers, config) {
+            console.log(data);
+            console.log(status);
+        });
+    }
+    $http.get(url).success(function (data, status, headers, config) {
+        $scope.absolutePath = data.path;
+        $scope.goto();
+    }).error(function (data, status, headers, config) {
+        console.log(data);
+        console.log(status);
+    });
+});
